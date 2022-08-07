13d6ead0b876fb60a02cf74e7af9e5672c23c250
Ashwini Mankani <ashwinimankani.xwokz@gmail.com>
2022-08-07 12:58:32 +0530

    commiting files

---
+++ table.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE xworkz;
+use xworkz;
+CREATE  TABLE games(id int, name varchar(20));
+SELECT * FROM games;
+INSERT INTO games values (1, "pubg");
+INSERT INTO games VALUES(2, "Candycrash");
+INSERT INTO games values (3, "temple run");
+INSERT INTO games VALUES(4, "ludo");
+Insert into games values (5, "puneet")
+
